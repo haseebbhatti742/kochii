dbaf94d66fac526c852dc26f8390b0f0ff7bfb18
haseebbhatti742 <haseebbhatti742@github.com>
2020-10-19 12:58:17 +0500

    chuss updated

--- routes/client/appointments.js
+++ routes/client/appointments.js
@@ -21,50 +21,10 @@ router.get("/", function(req, res) {
             if (data.status == "ok") {
                 res.render("client/appointments", { appointments: data.dataArray });
             } else if (data.status == "error") {
-                res.send("uuuhhhh")
-                    // res.render("client/appointments", { appointments: data.dataArray });
+                errorMessage = "Error: " + data.errorMessage;
+                res.redirect("/error");
             }
         })
-
-        // var query = "SELECT * from appointments WHERE user_id = '" + req.session.username + "' order by appointment_id desc";
-        // var appointments = [];
-        // app.conn.query(query, function(err, result1) {
-        //     if (err) {
-        //         console.log(err.message);
-        //         errorMessage = "Error: " + err.message;
-        //         //res.redirect("/error");
-        //     } else if (result1.length == 0) {
-        //         console.log("No appointment");
-        //         errorMessage = "Error: No appointment";
-        //         //res.redirect("/error");
-        //     } else if (result1.length > 0) {
-        //         for (let i = 0; i < result1.length; i++) {
-        //             appointments.push({ "appointments": result1[i] });
-        //         }
-
-        //         for (let i = 0; i < appointments.length; i++) {
-        //             var query2 = "SELECT * from appointment_services JOIN services ON appointment_services.service_id = services.service_id WHERE appointment_services.appointment_id=" + appointments[i].appointments.appointment_id;
-        //             app.conn.query(query2, function(err, result2) {
-        //                 if (err) {
-        //                     console.log(err.message);
-        //                     errorMessage = "Error: " + err.message;
-        //                     //res.redirect("/error");
-        //                 } else if (result2.length == 0) {
-        //                     console.log("No appointment");
-        //                     errorMessage = "Error: No Appointments";
-        //                     //res.redirect("/error");
-        //                 } else if (result2.length > 0) {
-        //                     appointments[i].appointment_services = result2;
-        //                     // console.log(appointments[i]);
-        //                 }
-        //             })
-        //         }
-        //         setTimeout(function() {
-        //             // console.log(appointments);
-        //             res.render("client/appointments", { appointments: appointments });
-        //         }, 1000);
-        //     }
-        // });
     }
 });
 
@@ -106,7 +66,7 @@ router.get("/mobile/:username", function(req, res) {
             res.status(200).json({ status: "error", errorMessage: err.message });
         } else if (result1.length == 0) {
             console.log("No appointment");
-            res.status(200).json({ status: "error", errorMessage: "No appointment" });
+            res.status(200).json({ status: "ok", dataArray: appointments, errorMessage: "No appointment" });
         } else if (result1.length > 0) {
             for (let i = 0; i < result1.length; i++) {
                 appointments.push({ "appointments": result1[i] });
@@ -119,8 +79,8 @@ router.get("/mobile/:username", function(req, res) {
                         console.log(err.message);
                         res.status(200).json({ status: "error", errorMessage: err.message });
                     } else if (result2.length == 0) {
-                        console.log("No appointment");
-                        res.status(200).json({ status: "error", errorMessage: "No appointment" });
+                        console.log("No appointment services");
+                        res.status(200).json({ status: "ok", dataArray: appointments });
                     } else if (result2.length > 0) {
                         appointments[i].appointment_services = result2;
                         // console.log(appointments[i]);
--- routes/client/error.js
+++ routes/client/error.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const app = require("../../app");
 
 router.get("/", function(req, res) {
-    if (req.session.username != null && req.session.type == "client")
-        res.redirect("/home");
-    else if (req.session.username != null && req.session.type == "manager")
+    if (req.session.username != null && req.session.type == "client") {
+        res.locals.title = "Error";
+        res.render("client/error");
+    } else if (req.session.username != null && req.session.type == "manager")
         res.redirect("/manager/home")
     else if (req.session.username != null && req.session.type == "receptionist")
         res.redirect("/receptionist/home")
     else if (req.session.username == null) {
-        res.locals.title = "";
+        res.locals.title = "Error";
         res.render("client/error");
     }
 });
