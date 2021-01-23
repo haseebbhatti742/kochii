920e0fcec84a5b4dd5f8f70d607da6d9b9725d91
haseebbhatti742 <59063435+haseebbhatti742@users.noreply.github.com>
2021-01-23 16:08:00 +0500

    df

--- routes/client/profile.js
+++ routes/client/profile.js
@@ -5,7 +5,8 @@ const app = require("../../app");
 router.get("/", function(req, res) {
     if (req.session.username != null && req.session.type == "client") {
         res.locals.title = "Profile";
-        res.render("client/profile");
+        res.send("Profile Page")
+            //res.render("client/profile");
     } else if (req.session.username != null && req.session.type == "manager")
         res.redirect("/manager/home")
     else if (req.session.username != null && req.session.type == "receptionist")
